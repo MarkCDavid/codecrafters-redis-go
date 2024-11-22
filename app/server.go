8ccf83896b7afca1b91e73748ab1516ab8936764
Mark David <sakalysaurimas@gmail.com>
2024-11-22 23:02:01 +0200

    Stage 2 - Respond with Pong

--- app/server.go
+++ app/server.go
@@ -6,17 +6,21 @@ import (
 	"os"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+const (
+	pingResponse = "+PONG\r\n"
+)
 
+func main() {
 	socket, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = socket.Accept()
+	connection, err := socket.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	connection.Write([]byte(pingResponse))
 }
