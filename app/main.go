12bb3704983e58a79deb461bd5d4e961d64edd52
Aurimas Šakalys <aurimas.sakalys@vinted.com>
2025-03-17 14:53:00 +0200

    Respond with PONG

--- app/main.go
+++ app/main.go
@@ -7,14 +7,34 @@ import (
 )
 
 func main() {
+	fmt.Println("Listening on 0.0.0.0:6379...")
 	socket, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = socket.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		connection, err := socket.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		connection.Write(AsSimpleString("PONG"))
 	}
+}
+
+// https://redis.io/docs/latest/develop/reference/protocol-spec/#simple-strings
+func AsSimpleString(value string) []byte {
+	simpleString := make([]byte, len(value)+3)
+	simpleString[0] = byte('+')
+	for index, character := range value {
+		simpleString[index+1] = byte(character)
+	}
+
+	simpleString[len(simpleString)-2] = byte('\r')
+	simpleString[len(simpleString)-1] = byte('\n')
+
+	return simpleString
 }
--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         buildInputs = with pkgs; [
           go
           just
+          redis
         ];
       };
     });
